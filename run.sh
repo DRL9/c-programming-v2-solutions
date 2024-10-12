76064b9921aaddda195789957e2445778610d1b4
Yuanjun Zhong <937676516@qq.com>
2024-10-12 13:53:39 +0800

    init

---
+++ run.sh
@@ -0,0 +1,5 @@
+cmakefile="./CMakeLists.txt"
+# 删除原来的行
+sed -i '' '/add_executable.*/d' $cmakefile
+# 添加全量的行
+find src -name *.c -type f | sort | awk -F / '{print "add_executable("$2 "_" $3 " " $0")"}' >>$cmakefile
